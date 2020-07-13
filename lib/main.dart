eb97d093873bb6905706bde4fbeab4f35a1831d7
Leonardo Freire <Leonardo13022019@gmail.com>
2020-07-13 12:30:45 -0300

    Upload Files

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,                 
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeWidget(),
+    );
+  }
+}
+
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
